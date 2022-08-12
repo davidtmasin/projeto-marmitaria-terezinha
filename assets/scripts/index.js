1e129d87736ddd0cc77f02e6f0cd5651149cc53d
David Teixeira de Masin <dtmasin@sfiec.org.br>
2022-08-12 15:58:22 -0300

    Lógica para personalizar mensagem do Whatsapp

---
+++ assets/scripts/index.js
@@ -0,0 +1,107 @@
+let checkboxes = document.getElementsByName("itemCardapio")
+let selecionados = 0
+let pedidoWpp = document.getElementById("pedido")
+let meals = document.querySelectorAll(".meal")
+let descriptions = document.querySelectorAll(".description")
+let prices = document.querySelectorAll(".price")
+let pedidoCompleto = "Olá, bom dia, poderia me ajudar com meu pedido?\n\n"
+let buttonLimpar = document.getElementById("limpar")
+
+pedidoWpp.addEventListener("click", function (e) {
+    let selecionados = 0
+    let marmita = ""
+    let descricao = ""
+    let preco = ""
+    let msgWhatsapp = ""
+
+    checkboxes.forEach(function (el) {
+        if (el.checked) {
+            selecionados++
+            // console.log(el.id);
+            if (el.id == "marmita01") {
+                console.log("Marmita 01");
+                // meals.forEach(function (ele) {
+                //   console.log(ele.textContent);
+                // })
+                // console.log(meals[0].textContent);
+                marmita = meals[0].textContent
+                // console.log(descriptions[0].textContent);
+                descricao = descriptions[0].textContent
+                // console.log(prices[0].textContent);
+                preco = prices[0].textContent
+
+                pedidoCompleto += `Eu gostaria de:\n*${marmita} - ${preco}*\n`
+                console.log(pedidoCompleto)
+                msgWhatsapp = pedidoCompleto
+                msgWhatsapp = window.encodeURIComponent(msgWhatsapp)
+                pedidoWpp.href = `https://wa.me/5585989647993?text=${msgWhatsapp}`
+            }
+
+            if (el.id == "marmita02") {
+                console.log("Marmita 02");
+                // meals.forEach(function (ele) {
+                //   console.log(ele.textContent);
+                // })
+                // console.log(meals[1].textContent);
+                marmita = meals[1].textContent
+                // console.log(descriptions[1].textContent);
+                descricao = descriptions[1].textContent
+                // console.log(prices[1].textContent);
+                preco = prices[1].textContent
+
+                pedidoCompleto += `Eu gostaria de:\n*${marmita} - ${preco}*\n`
+                console.log(pedidoCompleto)
+                msgWhatsapp = pedidoCompleto
+                msgWhatsapp = window.encodeURIComponent(msgWhatsapp)
+                pedidoWpp.href = `https://wa.me/5585989647993?text=${msgWhatsapp}`
+            }
+
+            if (el.id == "marmita03") {
+                console.log("Marmita 03");
+                // meals.forEach(function (ele) {
+                //   console.log(ele.textContent);
+                // })
+                // console.log(meals[2].textContent);
+                marmita = meals[2].textContent
+                // console.log(descriptions[2].textContent);
+                descricao = descriptions[2].textContent
+                // console.log(prices[2].textContent);
+                preco = prices[2].textContent
+
+                pedidoCompleto += `Eu gostaria de:\n*${marmita} - ${preco}*\n`
+                console.log(pedidoCompleto)
+                msgWhatsapp = pedidoCompleto
+                msgWhatsapp = window.encodeURIComponent(msgWhatsapp)
+                pedidoWpp.href = `https://wa.me/5585989647993?text=${msgWhatsapp}`
+            }
+
+            if (el.id == "marmita04") {
+                console.log("Marmita 04");
+                // meals.forEach(function (ele) {
+                //   console.log(ele.textContent);
+                // })
+                // console.log(meals[3].textContent);
+                marmita = meals[3].textContent
+                // console.log(descriptions[3].textContent);
+                descricao = descriptions[3].textContent
+                // console.log(prices[3].textContent);
+                preco = prices[3].textContent
+
+                pedidoCompleto += `Eu gostaria de:\n*${marmita} - ${preco}*\n`
+                console.log(pedidoCompleto)
+                msgWhatsapp = pedidoCompleto
+                msgWhatsapp = window.encodeURIComponent(msgWhatsapp)
+                pedidoWpp.href = `https://wa.me/5585989647993?text=${msgWhatsapp}`
+            }
+
+        }
+    })   // console.log(selecionados);
+}
+)
+
+buttonLimpar.addEventListener("click", function (e) {
+    pedidoWpp.href = "https://wa.me/5585989647993?text=Olá, bom dia, poderia me ajudar com meu pedido?"
+    checkboxes.forEach(function (el) {
+        el.checked = false
+    })
+})
