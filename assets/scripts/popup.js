f0e56f65c00a170d948634c58a59407a077ceb77
David Teixeira de Masin <dtmasin@sfiec.org.br>
2022-08-19 09:53:37 -0300

    Melhoria no POPUP 2

--- assets/scripts/popup.js
+++ assets/scripts/popup.js
@@ -1,25 +1,39 @@
 var close = document.getElementById('close');
 var popup = document.getElementById('popup');
 
-close.addEventListener("click", function() {
-  popup.style.display = 'none';
-});
-
-
-function loadPoup(){
+close.addEventListener("click", function () {
+    // popup.style.display = 'none';
     setTimeout(() => {
-        popup.style.opacity = '0.3';
-    }, 1000);
+        popup.style.opacity = '0.9';
+    }, 1000)
 
     setTimeout(() => {
         popup.style.opacity = '0.6';
-    }, 1000);
+    }, 1000)
 
     setTimeout(() => {
         popup.style.opacity = '0.9';
-    }, 1000);
+    }, 1000)
+    setTimeout(() => {
+        popup.style.opacity = '0';
+    }, 1000)
+})
+
+
+function loadPoup() {
+    setTimeout(() => {
+        popup.style.opacity = '0.3';
+    }, 1000)
+
+    setTimeout(() => {
+        popup.style.opacity = '0.6';
+    }, 1000)
+
+    setTimeout(() => {
+        popup.style.opacity = '0.9';
+    }, 1000)
 
     setTimeout(() => {
         popup.style.opacity = '1';
-    }, 1000);
+    }, 1000)
 }
